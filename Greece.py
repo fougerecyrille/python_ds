3a80586187d3f7d5a07eeeefac69f878016a99d6
fougerecyrille <cyrille.fougere@ensae.fr>
2023-12-25 23:01:06 +0100

    Converson xlsx->panda_df

---
+++ Greece.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pynsee.download
+
+import pandas as pd
+
+excel_file_path = "/Users/cyrillefougere/Desktop/ENSAE 2023:2024/S1/Python et Data Science/Databases/Key farm indicators_Greece_NUTS2.xlsx"
+ttl_hold_nb = "Sheet 1"  
+df_hold = pd.read_excel(excel_file_path, sheet_name=ttl_hold_nb, engine='openpyxl')
+
+print(df_hold)
