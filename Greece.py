ea2e703fd420f9727ca5593a4a0670650275f512
fougerecyrille <you@exa/usr/bin/python3 /Users/cyrillefougere/python_ds/Essais.pymple.com>
2023-12-26 09:42:24 +0100

    Contrôle du code pour le pie chart, ttl_hold_nb, Greece

--- Greece.py
+++ Greece.py
@@ -3,7 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pynsee.download
 
-import pandas as pd
+"""1. Drawing a pie chart illustrating the geographic repartition of the agricultural holdings in Greece,
+by number of holdings and European NUTS2 regions"""
 
 excel_file_path = "/Users/cyrillefougere/Desktop/ENSAE 2023:2024/S1/Python et Data Science/Databases/Key farm indicators_Greece_NUTS2.xlsx"
 ttl_hold_nb = "Sheet 1"  
@@ -18,6 +19,6 @@ df_hold_modified = df_hold.iloc[1:]
 print(df_hold_modified)
 labels = df_hold_modified['Geographic indication']
 sizes = df_hold_modified['Number of holdings']
-plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+plt.pie(sizes, labels=labels, autopct='%1.1f%%')
 plt.title('Répartition géographique des exploitations')
 plt.show()
