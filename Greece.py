f42f01c91ae5772a351756beada9191f2f54ba96
fougerecyrille <you@exa/usr/bin/python3 /Users/cyrillefougere/python_ds/Essais.pymple.com>
2023-12-26 10:08:29 +0100

    Bar chart, used agricultural area (ha) by NUTS2 regions

--- Greece.py
+++ Greece.py
@@ -22,3 +22,23 @@ sizes = df_hold_modified['Number of holdings']
 plt.pie(sizes, labels=labels, autopct='%1.1f%%')
 plt.title('Répartition géographique des exploitations')
 plt.show()
+
+"""2. Drawing a bar chart illustrating the total used agricultural area in Greece, by ha. and European NUTS2 regions"""
+
+excel_file_path = "/Users/cyrillefougere/Desktop/ENSAE 2023:2024/S1/Python et Data Science/Databases/Key farm indicators_Greece_NUTS2.xlsx"
+ttl_area_ha = "Sheet 2"  
+df_area = pd.read_excel(excel_file_path, sheet_name=ttl_area_ha, engine='openpyxl')
+
+# Replacing '(NUTS 2010)' with an empty string in 'Geographic indication'
+df_area['Geographic indication'] = df_area['Geographic indication'].str.replace(' (NUTS 2010)', '')
+print(df_area)
+
+# Drawing a bar chart according to these data
+plt.bar(df_area['Geographic indication'],df_area['Used agricultural area (ha)'])
+plt.xlabel('Geographic indication')
+plt.ylabel('Used agricultural area (ha)')
+plt.title('Used Agricultural Area in Greece by NUTS2 Regions')
+for i, value in enumerate(df_area['Used agricultural area (ha)']):
+    plt.text(i, value + 0.1, str(value), ha='center', va='bottom', rotation=45)
+plt.xticks(rotation=45, ha='right')
+plt.show()
